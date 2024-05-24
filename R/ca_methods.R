8fc4eef46b6cc3d29e3da3a2bae6def02d79506f
ophiase <ophiase@outlook.com>
2024-05-24 10:26:00 +0200

    added columns of augment have to be prepend with a dot

--- R/ca_methods.R
+++ R/ca_methods.R
@@ -5,7 +5,7 @@
 generics::augment
 
 random_method <- function() {
-  return("hello world");
+  return("hello world")
 }
 
 #' Augment data with information from a CA object
@@ -33,25 +33,25 @@ augment.CA <- function(x, data, ...) {
 
   # r$row
   if (!is.null(x$row)) {
-    data$coord <- unlist(x$row$coord)
-    data$cos2 <- unlist(x$row$cos2)
-    data$contrib <- unlist(x$row$contrib)
-    data$inertia <- unlist(x$row$inertia)
+    data$.coord <- unlist(x$row$coord)
+    data$.cos2 <- unlist(x$row$cos2)
+    data$.contrib <- unlist(x$row$contrib)
+    data$.inertia <- unlist(x$row$inertia)
   }
 
   # x$call
   if (!is.null(x$call)) {
     if (!is.null(x$call$X)) {
-      data$X <- x$call$X
+      data$.X <- x$call$X
     }
     if (!is.null(x$call$marge.row)) {
-      data$marge.row <- unlist(x$call$marge.row)
+      data$.marge.row <- unlist(x$call$marge.row)
     }
     if (!is.null(x$call$row.w)) {
-      data$row.w <- unlist(x$call$row.w)
+      data$.row.w <- unlist(x$call$row.w)
     }
     if (!is.null(x$call$Xtot)) {
-      data$Xtot <- x$call$Xtot
+      data$.Xtot <- x$call$Xtot
     }
   }
 
